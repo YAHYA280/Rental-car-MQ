cb758375b10e25ac166058027e69535af4fb8d95
yahyaELMOKHTARI <elmokhtariyahya2001@gmail.com>
2025-08-21 14:40:36 +0100

    updated hald working

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,15 +2,10 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ["en", "fr"],
-  // Used when no locale matches
   defaultLocale: "en",
-  // Always show locale prefix
-  localePrefix: "always",
+  localePrefix: "as-needed",
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
